50e5a15942ace2a6cb3b88396d7c25811fc53989
bartekwichowski <bartekwichowski@gmail.com>
2017-05-22 13:46:46 +0200

    Merge branch 'master' of C:\Users\Bartek\IdeaProjects\runthroughkeywords with conflicts.

---
+++ src/main/java/lucenereview/Constants.java
@@ -0,0 +1,9 @@
+package lucenereview;
+
+/**
+ * Created by Bartek on 5/22/2017.
+ */
+public class Constants {
+    public static final String PATH = "resources/potop-tom-pierwszy.txt";
+    public static final String INDEX_DIR = "LUCENE_DIR";
+}
